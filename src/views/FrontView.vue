4f3652fd81ad1ec35044ce3fdf74c79f9740c2b1
sihle <gubug777@gmail.com>
2022-05-01 19:50:44 +0800

    新增 Navbar 元件

---
+++ src/views/FrontView.vue
@@ -0,0 +1,14 @@
+<template>
+  <Navbar />
+  <div class="container">Front page</div>
+</template>
+
+<script>
+import Navbar from '@/components/NavbarComponent.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
