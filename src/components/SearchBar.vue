6d062b483d778c7f127868e95ef8759035047f3c
sihle <gubug777@gmail.com>
2022-05-03 18:55:47 +0800

    更新 Searchbar 表單元件樣式

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex">
     <select
-      class="form-select rounded-0 border-2 border-dark py-3 px-4 me-3"
+      class="form-select rounded-0 border-2 border-dark font-azeret py-3 px-4 me-3"
       aria-label="Default select"
       style="max-width: 156px"
     >
@@ -11,7 +11,7 @@
     <div class="input-group">
       <input
         type="text"
-        class="form-control rounded-0 border-end-0 border-2 border-dark py-3 px-4"
+        class="form-control rounded-0 border-end-0 border-2 border-dark font-azeret py-3 px-4"
         placeholder="搜尋貼文"
         aria-label="搜尋貼文"
         aria-describedby="search-button"
