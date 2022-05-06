c309fb3bf5a49e84c651836dc9420e7019ac9be0
sihle <gubug777@gmail.com>
2022-05-06 13:41:11 +0800

    新增 取得所有貼文 api

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,20 +1,19 @@
 <template>
   <Searchbar class="mb-4" />
   <ul>
-    <li class="card border-2 border-dark shadow-card">
+    <li v-for="post in posts" :key="post._id" class="card border-2 border-dark shadow-card mb-4">
       <div class="card-body p-6">
         <div class="d-flex mb-4">
           <img src="@/assets/images/user.png" alt="avatar" class="avatar rounded-circle me-4" />
           <div class="d-flex flex-column justify-content-center">
-            <h2 class="fs-6">邊緣小杰</h2>
-            <p class="fs-12 text-secondary">2022/1/10 12:00</p>
+            <h2 class="fs-6">{{ post.user.name }}</h2>
+            <p class="fs-12 text-secondary">{{ post.createdAt }}</p>
           </div>
         </div>
-        <p class="mb-4">外面看起來就超冷.... 我決定回被窩繼續睡....</p>
-        <img
-          src="@/assets/images/image.png"
-          alt="image"
-        />
+        <p class="mb-4">{{ post.content }}</p>
+        <div v-if="post.photo">
+          <img :src="post.photo" alt="image" />
+        </div>
       </div>
     </li>
   </ul>
@@ -26,6 +25,24 @@ import Searchbar from '@/components/SearchBar.vue';
 export default {
   components: {
     Searchbar,
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    getPosts() {
+      const api = 'http://localhost:3000/posts';
+      this.$http.get(api).then((res) => {
+        if (res.data.status === 'success') {
+          this.posts = res.data.data;
+        }
+      });
+    },
+  },
+  mounted() {
+    this.getPosts();
   },
 };
 </script>
