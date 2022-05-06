7fed0acf47e0c8c1c183eb89f2ba87e50ea20229
sihle <gubug777@gmail.com>
2022-05-06 13:15:12 +0800

    新增 動態牆樣板

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,6 +1,23 @@
 <template>
-  <Searchbar />
-  <h1>動態牆</h1>
+  <Searchbar class="mb-4" />
+  <ul>
+    <li class="card border-2 border-dark shadow-card">
+      <div class="card-body p-6">
+        <div class="d-flex mb-4">
+          <img src="@/assets/images/user.png" alt="avatar" class="avatar rounded-circle me-4" />
+          <div class="d-flex flex-column justify-content-center">
+            <h2 class="fs-6">邊緣小杰</h2>
+            <p class="fs-12 text-secondary">2022/1/10 12:00</p>
+          </div>
+        </div>
+        <p class="mb-4">外面看起來就超冷.... 我決定回被窩繼續睡....</p>
+        <img
+          src="@/assets/images/image.png"
+          alt="image"
+        />
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -12,3 +29,10 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.avatar {
+  width: 45px;
+  height: 45px;
+}
+</style>
