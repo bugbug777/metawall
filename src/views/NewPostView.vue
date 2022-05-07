61cc8e1f40f4bb75928817bc73c97cf3a3544cec
sihle <gubug777@gmail.com>
2022-05-07 18:29:22 +0800

    新增 新增貼文樣板

--- src/views/NewPostView.vue
+++ src/views/NewPostView.vue
@@ -1,3 +1,66 @@
 <template>
-  <h1>新增貼文</h1>
+  <div class="position-relative box-shift mb-4">
+    <h2 class="position-relative border border-2 border-dark fs-5 text-center bg-white py-4">
+      張貼動態
+    </h2>
+  </div>
+  <div class="card border-2 border-dark">
+    <div class="card-body p-8">
+      <form action="">
+        <div class="mb-4">
+          <label for="content" class="form-label mb-1">貼文內容</label>
+          <textarea
+            name="content"
+            id="content"
+            rows="6"
+            placeholder="輸入您的貼文內容"
+            class="form-control rounded-0 border-2 border-dark px-4 py-3"
+          ></textarea>
+        </div>
+        <div class="mb-4">
+          <label for="imageUrl" class="form-label mb-1">圖片網址</label>
+          <input
+            class="form-control border-2 border-dark px-4"
+            type="text"
+            name="imageUrl"
+            id="imageUrl"
+            placeholder="請輸入圖片網址"
+          />
+        </div>
+        <img class="mb-8" src="@/assets/images/image.png" alt="image" />
+        <input
+          class="d-block w-60 btn btn-gray-2 btn-hover border-2 border-dark fw-bold py-4 mx-auto"
+          type="submit"
+          value="送出貼文"
+        />
+      </form>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.box-shift {
+  &::before {
+    display: block;
+    content: '';
+    position: absolute;
+    top: 6px;
+    right: 6px;
+    bottom: -6px;
+    left: -6px;
+    border: 2px solid #000400;
+    background-color: #fff;
+    z-index: 1;
+  }
+  h2 {
+    z-index: 2;
+  }
+}
+.btn-hover:hover {
+  background-color: #EEC32A;
+  box-shadow: -2px 2px 0px #000400;
+}
+.w-60 {
+  width: 60%;
+}
+</style>
